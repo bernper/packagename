2f6664334a795f1f092017126f30df316fe8321c
bernper <bperanec@gmail.com>
2020-10-23 12:15:30 +0200

    Initial commit

---
+++ R/my_add_function.R
@@ -0,0 +1,26 @@
+
+
+#' Adding Numbers and Square
+#' @param x numeric: the first parameter in my add and square functio
+#' @param y numeric: the second parameter
+#' @rdname add
+add <- function(x,y){
+  x+y
+}
+
+#' @rdname add
+add2 <- function(x,y){
+  x+y
+}
+
+#' @rdname add
+add3 <- function(x,y){
+  x+y
+}
+
+#' @rdname add
+add_quantiles <- function(x,y){
+    quantile(x+y)
+
+}
+
